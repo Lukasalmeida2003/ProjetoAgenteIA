75ebd22362080bf4a15abf9146adddad6302f001
Lukasalmeida2003 <83170095+Lukasalmeida2003@users.noreply.github.com>
2025-06-09 16:31:42 -0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import { BrainCircuit, Zap, Target, Clock } from 'lucide-react';
 
 const Hero: React.FC = () => {
   const [typedText, setTypedText] = useState('');
-  const fullText = "seem pensar, sem errar, sem enrolar.";
+  const fullText = "Sem pensar, sem errar, sem enrolar.";
   
   useEffect(() => {
     let i = 0;
